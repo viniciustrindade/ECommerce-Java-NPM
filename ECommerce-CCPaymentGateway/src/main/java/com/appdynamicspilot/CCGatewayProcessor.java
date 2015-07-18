412ad7eb1f804702a7888659b35587ca8cbffbcb
shivloka <shiv.loka@appdynamics.com>
2015-07-18 02:30:40 -0700

    added required changes for cxf to ccpayment rest service integration, removed some redudant libraries causing exception in ECommerce-WS, added correct database properties - username and password

--- ECommerce-CCPaymentGateway/src/main/java/com/appdynamicspilot/CCGatewayProcessor.java
+++ ECommerce-CCPaymentGateway/src/main/java/com/appdynamicspilot/CCGatewayProcessor.java
@@ -1,13 +1,13 @@
 package com.appdynamicspilot;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Random;
 
 /**
  * Created by shiv.loka on 7/7/2015.
@@ -15,38 +15,24 @@ import org.slf4j.LoggerFactory;
 
 @Path("/webpay")
 public class CCGatewayProcessor {
-	
-	private long ccNumber, orderId;
-	private String username;
-	private boolean returnFailure;
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(CCGatewayProcessor.class);
 
 
 	@GET
-	@Path("/cc-pay/{ccNumber}/{userName}/{orderId}/{fail}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String processPayment(@PathParam("userName") String username,
-			@PathParam("ccNumber") long ccNumber,@PathParam("orderId") long orderId, @PathParam("fail") boolean returnFailure) throws Exception {
-		this.ccNumber = ccNumber;
-		this.returnFailure = returnFailure;
-		this.username = username;
-		this.orderId = orderId;
-		
-		logger.info("credit card number is:" +this.ccNumber);
-		logger.info("Order : " + this.orderId + " has been processed for the user: " +this.username);
+	@Path("/ccpay")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String processPayment() {
+		Random randInteger = new Random();
+		int randomNumber = randInteger.nextInt(10);
 
-		if (this.returnFailure) {
-			throw new Exception("Sorry, failed to process card");
+		if (randomNumber % 2 == 0) {
+			return "success";
 		}
-		return "Order : " + this.orderId + " has been processed for the user: " +this.username;
+		else
+			return "failure";
 
 	}
 
-
-	protected void generateException() throws Exception {
-
-		Exception e = new Exception();
-		throw e;
-	}
 }
